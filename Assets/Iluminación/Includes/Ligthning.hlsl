5097296cd47b81a5d29edab94eeff845eba411bb
Jeronimo Mejia <110081118+Jero111@users.noreply.github.com>
2024-09-25 19:33:47 -0500

    Luces y más

--- Assets/Iluminación/Includes/Ligthning.hlsl
+++ Assets/Iluminación/Includes/Ligthning.hlsl
@@ -1,4 +1,24 @@
+#ifndef PAPITA
+#define PAPITA
+
+//#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
+
+void GetMainLitghInfo_float(out float3 direction, out half3 color)
+{
+    
+    #if defined(SHADERGRAPH_PREVIEW)
+    direction = float3(1, 1, -1);
+    color = 1;
+    #else 
+    Light mainLight = GetMainLight();
+    direction = mainLight.direction;
+    color = mainLight.color;
+    #endif
+
+}
+
 void Add_float(float a, float b, out float c)
 {
     c = a + b;
 }
+#endif
